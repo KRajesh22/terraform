52d1709104535ae40e4359fd49863020d12d110d
Raghu K <raghuk.vit@gmail.com>
2018-12-03 01:05:40 +0000

    Updating student-proj-test-env

--- student-proj-test-env/rds.tf
+++ student-proj-test-env/rds.tf
@@ -18,6 +18,7 @@ resource "aws_db_instance" "default" {
   name                 = "studentapp"
   username             = "${var.dbuser}"
   password             = "${var.dbpass}"
+  skip_final_snapshot  = true
 }
 
 resource "null_resource" "dbschema" {
